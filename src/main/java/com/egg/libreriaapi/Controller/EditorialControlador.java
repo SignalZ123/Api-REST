0efea4a8017a59955bb35424ac28f907aeacd437
SignalZ123 <timcampy123@gmail.com>
2025-01-04 23:40:21 -0500

    se añadio metodos al controlador Editorial

--- src/main/java/com/egg/libreriaapi/Controller/EditorialControlador.java
+++ src/main/java/com/egg/libreriaapi/Controller/EditorialControlador.java
@@ -3,8 +3,10 @@ package com.egg.libreriaapi.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.egg.libreriaapi.servicios.EditorialServicios;
@@ -16,13 +18,22 @@ public class EditorialControlador {
     private EditorialServicios editorialServicios;
 
     @PostMapping("crear")
-    public ResponseEntity<Object> crearEditorial(String nombre){
+    public ResponseEntity<Object> crearEditorial(@RequestParam String nombre){
         try {
             editorialServicios.crearEditorial(nombre);
-            return new ResponseEntity<>(HttpStatus.OK);            
+            return new ResponseEntity<>("Editorial creada correctamente: ",HttpStatus.OK);            
         } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>("Error al crear una nueva editorial: ", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
     
+    @GetMapping("listar")
+    public ResponseEntity<Object> listarEditorial(){
+        try {
+            return ResponseEntity.ok(editorialServicios.listaEditoriales());
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error al listar las editoriales: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
 }
