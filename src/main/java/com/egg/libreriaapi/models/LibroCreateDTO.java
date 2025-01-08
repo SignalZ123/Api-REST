e049bd7394b0e63737fd92bbd70a575f656deaf9
SignalZ123 <timcampy123@gmail.com>
2025-01-07 23:55:56 -0500

    DTOLIBRO

---
+++ src/main/java/com/egg/libreriaapi/models/LibroCreateDTO.java
@@ -0,0 +1,22 @@
+package com.egg.libreriaapi.models;
+
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data//simplifica anadir los getter y setters y anade un consturctor
+/*(Objeto de transferencia de datos)
+ * empleado para la creacion de un nuevo libro.
+ * se diferencia de la 'Entidad Libro', no requiere alamcenar el objeto completo de Autor y Editorial. Si no solo sus ID, sim embargo conserva los demas atributos.
+ */
+
+public class LibroCreateDTO {
+
+    private Long isbn; //id del libro
+    private String titulo;
+    private int ejemplares;
+    private UUID idAutor;
+    private UUID idEditorial;
+    private boolean libroActivo;
+    
+}
