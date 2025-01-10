a913e630f9c806ab2f81f38bd18227f7995abdb6
SignalZ123 <timcampy123@gmail.com>
2025-01-09 23:58:29 -0500

    añadiendo models/Libro

---
+++ src/main/java/com/egg/libreriaapi/models/Libro/LibroListDTO.java
@@ -0,0 +1,18 @@
+package com.egg.libreriaapi.models.Libro;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LibroListDTO {
+    private Long isbnDTO;
+    private String tituloDTO;
+    private int ejemplaresDTO;
+    private UUID idAutor;
+    private UUID idEditorial;
+    private boolean libroActivoDTO;
+    
+}
