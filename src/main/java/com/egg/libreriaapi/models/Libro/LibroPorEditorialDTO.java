11792f06d0ff32fe5684730b3eb43e357234ab60
SignalZ123 <timcampy123@gmail.com>
2025-01-10 23:43:57 -0500

    agregado nueva clase LibroPorEditorialDTO

---
+++ src/main/java/com/egg/libreriaapi/models/Libro/LibroPorEditorialDTO.java
@@ -0,0 +1,32 @@
+package com.egg.libreriaapi.models.Libro;
+
+import java.util.UUID;
+
+import lombok.Data;
+
+//Estrelaciona como una entidad para el Libro Repositorio haga la consulta de obtener 'Libros existentes por editorial'
+//corroborar con el LibroRepositorio
+@Data
+public class LibroPorEditorialDTO {
+
+  
+    private Long idLibro;
+    private String titulo;
+    private int ejemplares;
+    private boolean libroActivo;
+    private UUID idAutor;
+    private UUID idEditorial;
+
+    //Constructor
+    public LibroPorEditorialDTO(Long idBook, String title, int ejemplares, boolean libroActiv, UUID idAut, UUID idEdi){
+
+        this.idLibro = idBook;
+        this.titulo = title;
+        this.ejemplares = ejemplares;
+        this.libroActivo = libroActiv;
+        this.idAutor = idAut;
+        this.idEditorial=idEdi;
+
+    }
+    
+}
