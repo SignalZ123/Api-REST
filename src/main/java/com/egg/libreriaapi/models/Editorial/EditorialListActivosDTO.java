b73e01f9770ea7325f5c1f3d2242d357a6d18a53
OmarDev <timcampy123@gmail.com>
2025-01-13 21:56:12 -0500

    añaaido el EditorialListActivosDTO

---
+++ src/main/java/com/egg/libreriaapi/models/Editorial/EditorialListActivosDTO.java
@@ -0,0 +1,13 @@
+package com.egg.libreriaapi.models.Editorial;
+
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
+public class EditorialListActivosDTO {
+    private UUID idEditorial;
+    private String nombreEditorial;
+    private boolean editorialActivo;
+    
+}
