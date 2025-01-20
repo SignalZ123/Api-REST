5727f7ed5939e34f6fb48e7a745544bb6e760b52
OmarDev <timcampy123@gmail.com>
2025-01-19 23:53:29 -0500

    nuevos meetodos para el controlador Libro

--- src/main/java/com/egg/libreriaapi/Controller/LibroControlador.java
+++ src/main/java/com/egg/libreriaapi/Controller/LibroControlador.java
@@ -7,18 +7,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.egg.libreriaapi.entidades.Libro;
 import com.egg.libreriaapi.models.Libro.LibroCreateDTO;
 import com.egg.libreriaapi.models.Libro.LibroListDTO;
 import com.egg.libreriaapi.models.Libro.LibroListarActivosDTO;
+import com.egg.libreriaapi.models.Libro.LibroPorAutorDTO;
+import com.egg.libreriaapi.models.Libro.LibroPorEditorialDTO;
 import com.egg.libreriaapi.servicios.LibroServicios;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
-
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -70,18 +72,80 @@ public class LibroControlador {
         }
     }
 
+    @GetMapping("listarTodoLibros")
+    public ResponseEntity<List<LibroListDTO>>  listarLibros() {
+        try {
+            List<LibroListDTO> librosLista =libroServicios.listarLibros();
+            return ResponseEntity.ok(librosLista);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
     @GetMapping("listartodoLibrosDTO")
     public ResponseEntity<List<LibroListDTO>> listarLibrosDTO() {
         try {
             // va contener un varios objetos de tipo 'LibroLisDTO' es como decir que ba contener objetos de tipo 'Libro' ya explicado anteriormente por DTO.
-            List<LibroListDTO> libroAll = libroServicios.listarLibros();
-
+            List<LibroListDTO> libroAll = libroServicios.listarLibrosDTO();
+            
             return ResponseEntity.ok(libroAll);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
 
     }
+
+    //probar controlador -------------------------------
+    @GetMapping("librosPorId/{idLibro}")
+    public ResponseEntity<Libro> obtenerLibrosPorID(@PathVariable long idLibro) {
+        try {
+            Libro librosId = libroServicios.obtenerLibroPorId(idLibro);
+            return ResponseEntity.ok(librosId);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("listarLibrosPorAutor/{idAutor}")
+    public ResponseEntity<List<LibroPorAutorDTO>> libroPoridAutor(@RequestParam UUID idAutor) {
+        try {
+            List<LibroPorAutorDTO> libroPoridAutor = libroServicios.listarLibrosPorAutor(idAutor);
+            return ResponseEntity.ok(libroPoridAutor);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("listarLibrosporEditorial/{idEditorial}")
+    public ResponseEntity<List<LibroPorEditorialDTO>> listarLibroPorEditorial(@RequestParam UUID idEditorial) {
+        try {
+            List<LibroPorEditorialDTO> libroPorEditorial = libroServicios.listarLibrosPorEditorial(idEditorial);
+            return ResponseEntity.ok(libroPorEditorial);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+    @GetMapping("listarPorEditorialyAutor")
+    public ResponseEntity<List<LibroPorAutorDTO>> listarLibroPorEditorialyAutor(@RequestParam UUID idAutor, @RequestParam UUID idEditorial) {
+        try {
+            List<LibroPorAutorDTO> libroPorEditorialyAutor = libroServicios.listarLibrosPorEditorialYLibros(idAutor, idEditorial);
+            return ResponseEntity.ok(libroPorEditorialyAutor);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PostMapping("darBajaLibro")
+    public ResponseEntity<Object> darBajaLibro(@RequestBody Long isbn) {
+        try {
+            
+            return ResponseEntity.ok(libroServicios.darBajaLibro(isbn));
+            
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+        
     
     
     
